1978a625bbe7e534bf77bacdc665c68e64231852
intermonino <intermonino@gmail.com>
2023-05-18 14:46:49 -0300

    [feat/week04] 4.3.2 added manager load service

---
+++ internal/services/manager-load/api.go
@@ -0,0 +1,16 @@
+package manager_load
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Pickausernaame/chat-service/internal/types"
+)
+
+func (s *Service) CanManagerTakeProblem(ctx context.Context, managerID types.UserID) (bool, error) {
+	count, err := s.problemsRepo.GetManagerOpenProblemsCount(ctx, managerID)
+	if err != nil {
+		return false, fmt.Errorf("geeting open problems count: %v", err)
+	}
+	return s.maxProblemsAtTime > count, nil
+}
